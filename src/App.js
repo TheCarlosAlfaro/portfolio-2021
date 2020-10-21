60cf558156246cda8c211cbe85942b09a3145e12
carlosalfarodev <rgkalin@gmail.com>
2020-10-20 18:54:52 -0700

    Adds node-sass for compiling sass files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./style/master.scss";
 
 function App() {
    return (
-      <div>
-         <h1>Hello world!</h1>
-      </div>
+      <>
+         <h1 className="text-danger">Hello world!</h1>
+      </>
    );
 }
 
